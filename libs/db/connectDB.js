8ee01983af14a99b226fa6a2393b26de70f46a20
Hari <harishnathr@gmail.com>
2024-10-16 09:54:52 +0530

    adjust

--- libs/db/connectDB.js
+++ libs/db/connectDB.js
@@ -1,9 +1,21 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 export async function connectDB() {
+    if (isConnected) {
+        console.log("Database is already connected.");
+        return;
+    }
+
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        isConnected = true;
+        console.log("Connected to MongoDB!");
     } catch (error) {
-        console.log("Error connecting to DB!", error);
+        console.error("Error connecting to DB!", error);
     }
 }
